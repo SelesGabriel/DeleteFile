1c4641c3e710ea01e042b8a3703e85b56d870176
gadasilvas <gabriel.seles@engdb.com.br>
2021-01-18 16:57:53 -0300

    Adicionar arquivos de projeto.

---
+++ DeleteFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace DeleteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diretorio = @"Diretorio";//colocar caminho do diretório
+            var nomeArquivos = Directory.EnumerateFiles(diretorio, "*", SearchOption.AllDirectories);
+            foreach (string nomeArquivo in nomeArquivos)
+            {
+                if (nomeArquivo.EndsWith("(1).jpg"))
+                {
+                    var arquivo = new FileInfo(nomeArquivo);
+                    arquivo.Delete();
+                }
+            }
+        }
+    }
+}
