37cf231b4fd170fb5480f7feda49e5bffd013995
gadasilvas <gabriel.seles@engdb.com.br>
2021-01-18 17:00:21 -0300

    Ajuste no comentário

--- DeleteFile/Program.cs
+++ DeleteFile/Program.cs
@@ -10,7 +10,7 @@ namespace DeleteFile
             var nomeArquivos = Directory.EnumerateFiles(diretorio, "*", SearchOption.AllDirectories);
             foreach (string nomeArquivo in nomeArquivos)
             {
-                if (nomeArquivo.EndsWith("(1).jpg"))
+                if (nomeArquivo.EndsWith("(1).jpg"))//detalhamento do arquivo
                 {
                     var arquivo = new FileInfo(nomeArquivo);
                     arquivo.Delete();
